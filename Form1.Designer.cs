34999d1139bb52f43b12074a06360fadae7cb208
Dev Dipankar Bera <dbera@charlotte.edu>
2026-05-02 15:57:08 -0400

    Changed the Form1

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pantry_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dgvInventory = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvInventory).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(92, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Item Name";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(93, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Quantity";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(93, 198);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Expiration Date";
+            label4.Click += label4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.AccessibleName = "txtName";
+            textBox1.Location = new Point(229, 57);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.AccessibleName = "txtQuantity";
+            textBox2.Location = new Point(229, 132);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(229, 193);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.AccessibleName = "btnAdd";
+            button1.Location = new Point(92, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 10;
+            button1.Text = "Add Item";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.AccessibleName = "btnUse";
+            button2.Location = new Point(247, 250);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 11;
+            button2.Text = "Use Item";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.AccessibleName = "btnExit";
+            button3.Location = new Point(426, 250);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 12;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dgvInventory
+            // 
+            dgvInventory.AccessibleName = "dgvInventory";
+            dgvInventory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvInventory.Location = new Point(93, 308);
+            dgvInventory.Name = "dgvInventory";
+            dgvInventory.RowHeadersWidth = 62;
+            dgvInventory.Size = new Size(446, 150);
+            dgvInventory.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AccessibleName = "dtpExpiry";
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 482);
+            Controls.Add(dgvInventory);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvInventory).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dgvInventory;
+    }
+}
